4b88f5d232aada223daf22b2582a58f02f31a4bf
LeiYinchun <a@wyolo.com>
2020-04-15 18:05:07 +0800

    add immer, immutable

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-import { IResponseData, TLottery, THolidayRecent } from './defines';
-import { apiAction } from './services';
+// type Pick<T, K extends keyof T> = {
+//   [P in K]: T[P];
+// };
 
-const retssq = apiAction('aimlottery', { expect: '2018135', code: 'ssq' });
-retssq.then(({ data }: IResponseData<TLottery>) => {
-  console.log(data);
-  // console.log(data.code);
-});
+enum PassengerType {
+  ADT = '成人',
+  CHD = '儿童',
+  INF = '婴儿',
+}
 
-const retholiday = apiAction('holidayrecent');
-retholiday.then(({ data }: IResponseData<THolidayRecent[]>) => {
-  // console.log(typeof data);
-  console.log(data);
-  // console.log(data[0].date);
-});
+class Identity {
+  static map = <T extends object>(o: T) => {
+    const ret = new Map();
+    for (const key in o) {
+      ret.set(o[key], key);
+    }
+    return ret;
+  };
 
-console.log(__dirname);
+  static get = <T extends object, K extends keyof T>(o: T, name: K) => {
+    return o[name];
+  };
+}
+
+console.log(Identity.map(PassengerType));
+console.log(Identity.get(PassengerType, 'ADT'));
+
+// const dad = Identity < PassengerType > console.log(__dirname);
+
+/**
+ * 1、新建一个空{}，检测Obj
+ * 2、先遍历tripProfiles，拿到origin、destination对应的一个节点A。
+ * 3、使用immer将存入Obj，即 { 'origin、destination': [A] }。如果之前Obj已有'origin、destination'，则追加。
+ * 4、最后将Obj追加到原始对象promotionDetailSummary上
+ */
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+// bootstrap();
